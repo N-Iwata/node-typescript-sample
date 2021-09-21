30d1471260db9441c5e5e13e1f1579f78bd484c0
Nobuhito Iwata <nobuhito323@gmail.com>
2021-09-21 12:23:07 +0900

    test

--- dist/app.js
+++ dist/app.js
@@ -5,3 +5,4 @@ console.log(ranking_1.getRankArrayOfNumbers([36000, 28000, 27000, 28000]));
 console.log(ranking_1.getRankArrayOfNumbers([80, 65, 90, 50]));
 console.log(ranking_1.getRankArrayOfNumbers([80, 80, 70, 50]));
 console.log(ranking_1.getRankArrayOfNumbers([80, 80, 80, 50]));
+//test

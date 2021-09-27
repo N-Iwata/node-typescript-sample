44f3738bf76ff9d1b6851e5dd8ec3439f7debc1a
Nobuhito Iwata <nobuhito323@gmail.com>
2021-09-27 13:51:57 +0900

    Remove dist

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ranking_1 = require("./ranking");
-console.log(ranking_1.getRankArrayOfNumbers([36000, 28000, 27000, 28000]));
-console.log(ranking_1.getRankArrayOfNumbers([80, 65, 90, 50]));
-console.log(ranking_1.getRankArrayOfNumbers([80, 80, 70, 50]));
-console.log(ranking_1.getRankArrayOfNumbers([80, 80, 80, 50]));
-//test

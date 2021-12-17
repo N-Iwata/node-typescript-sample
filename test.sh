b225194c09f3113c21d4f46bb89fdda24a2aab8b
Nobuhito Iwata <nobuhito323@gmail.com>
2021-12-17 16:15:56 +0900

    Fix test3

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 active_apps=("[deploy1]aaa" "[deploy2]bbb")
-PR_TITLE="[deploy2]bbb"
+PR_TITLE="[deploy3]aaaa"
 deployN=$(echo "${PR_TITLE}" | grep -o "deploy[0-9]" | cat)
 
 # for n in "${active_apps[@]}"
@@ -26,7 +26,8 @@ deployN=$(echo "${PR_TITLE}" | grep -o "deploy[0-9]" | cat)
 # done
 
 for ((i = 0; i < ${#active_apps[@]}; i++)) {
-    if [ ${active_apps[i]} != "${PR_TITLE}" ] && [[ ${active_apps[i]} == *${deployN}* ]]
+    echo "${active_apps[i]}"
+    if [ "${active_apps[i]}" != "${PR_TITLE}" ] && [[ "${active_apps[i]}" == *${deployN}* ]]
 then
     echo “${deployN} already exists”
     exit 1    

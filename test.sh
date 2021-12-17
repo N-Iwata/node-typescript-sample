0efd223a8989f034d2f0c9a9b72f990f761fd395
Nobuhito Iwata <nobuhito323@gmail.com>
2021-12-17 15:34:08 +0900

    Fix test3

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 
 active_apps=("[deploy1]aaa" "[deploy2]bbb")
 deployN="deploy2"
-PR_TITLE="[deploy2]ccc"
+PR_TITLE="[deploy2]bbb"
 
 
 for n in "${active_apps[@]}"
 do
-if [ ${n} != ${PR_TITLE} ] && [[ ${n} == *${deployN}* ]]
+if [ "${n}" != "${PR_TITLE}" ] && [[ ${n} == *${deployN}* ]]
 then
     echo “${deployN} already exists”
     exit 1    

28fc36bcae2d04ecef67d3ed6b2aadb282f2d43c
Nobuhito Iwata <nobuhito323@gmail.com>
2021-12-17 17:44:22 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,5 @@ console.log(getRankArrayOfNumbers([36000, 28000, 27000, 28000]));
 console.log(getRankArrayOfNumbers([80, 65, 90, 50]));
 console.log(getRankArrayOfNumbers([80, 80, 70, 50]));
 console.log(getRankArrayOfNumbers([80, 80, 80, 50]));
+
+// test

533aa391b0847196d1e06dc78af071aa7793aae0
Nobuhito Iwata <nobuhito323@gmail.com>
2021-12-17 16:44:26 +0900

    test

--- test.sh
+++ test.sh
@@ -5,27 +5,27 @@ active_apps='
 [deploy2]bbb
 '
 array_text=(`echo $active_apps`)
-# echo "${active_apps}"
-echo "${array_text[0]}"
-echo "${array_text[1]}"
-PR_TITLE="[deploy1]aaa"
+
+PR_TITLE="[deploy2]aaa"
 deployN=$(echo "${PR_TITLE}" | grep -o "deploy[0-9]" | cat)
 
-for i in "${!active_apps[@]}"
-do
-    if [ "${active_apps[${i}]}" != "${PR_TITLE}" ] && [[ "${active_apps[${i}]}" == *${deployN}* ]]
-    then
-        echo “${deployN} already exists”
-        exit 1    
-    fi
-done
+# for i in "${!active_apps[@]}"
+# do
+#     echo ${array_text[i]}
+#     echo ${PR_TITLE}
+#     if [ ${array_text[i]} != ${PR_TITLE} ] && [[ ${array_text[i]} == *${deployN}* ]]
+#     then
+#         echo “${deployN} already exists”
+#         exit 1    
+#     fi
+# done
 
 
-# for ((i = 0; i < ${!active_apps[@]}; i++)) {
-#     echo "${active_apps[${i}]}"
-#     if [ "${active_apps[${i}]}" != "${PR_TITLE}" ] && [[ "${active_apps[${i}]}" == *${deployN}* ]]
-# then
-#     echo “${deployN} already exists”
-#     exit 1    
-# fi
-# }
+for ((i = 0; i < ${#array_text[@]}; i++)) {
+    echo "${array_text[i]}"
+    if [ "${array_text[i]}" != "${PR_TITLE}" ] && [[ "${array_text[i]}" == *${deployN}* ]]
+then
+    echo “${deployN} already exists”
+    exit 1    
+fi
+}

065366d9d37428866e8103eec87e685a049adb82
Nobuhito Iwata <nobuhito323@gmail.com>
2021-12-17 15:20:06 +0900

    test-deploy2

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+active_apps=("[deploy1]aaa" "[deploy2]bbb")
+deployN="deploy2"
+PR_TITLE="[deploy2]ccc"
+
+for n in "${active_apps[@]}"
+do
+if [ ${n} != ${PR_TITLE} ] && [[ ${n} == *${deployN}* ]]
+then
+    echo “${deployN} already exists”
+    exit 1    
+fi
+done
